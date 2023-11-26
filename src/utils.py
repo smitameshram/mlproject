3bf74fa2b9412c48c8796f5f2aa44a0f382156c9
smita <me.smitameshram@gmail.com>
2023-11-27 01:48:25 +0530

    flask app and predict pipeline added

--- src/utils.py
+++ src/utils.py
@@ -48,3 +48,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+#to read pickle file
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
