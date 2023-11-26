5097c511dc9185d2a780edd2936cfb0dd95029f6
smita <me.smitameshram@gmail.com>
2023-11-26 20:07:28 +0530

    model trainer added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,8 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
 
 @dataclass #decorator: used when only variables have to be intialized
@@ -57,4 +58,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initite_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer= ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
